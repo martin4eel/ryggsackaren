ac3b93a56c64cf808ce7ef11a77a6475e94d10c6
Martin Karlsson <martin.karlsson@compeas.com>
2026-08-26 22:53:14 +0200

    Publicera spelet, byggt från b349939

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * har innehållshash i namnet och cachas därför permanent.
  */
 
-const CACHE = 'ryggsackaren-v4';
+const CACHE = 'ryggsackaren-v5';
 
 /**
  * Stadsfotona ligger med stabila namn under cities/ och hämtas in redan vid
