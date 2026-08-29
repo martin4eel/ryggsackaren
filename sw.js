d3de018e135f8b0978b73fb05f80c2a3df9c3a55
Martin Karlsson <martin.karlsson@compeas.com>
2026-08-29 08:44:49 +0200

    Publicera spelet, byggt från a42823d

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * har innehållshash i namnet och cachas därför permanent.
  */
 
-const CACHE = 'upptackaren-v29';
+const CACHE = 'upptackaren-v30';
 
 /**
  * Stadsfotona ligger med stabila namn under cities/ och hämtas in redan vid
