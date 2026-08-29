a07b9508ce4c62e832ff44cabf578614ea52e9c7
Martin Karlsson <martin.karlsson@compeas.com>
2026-08-29 21:06:12 +0200

    Skarpare klubbmärken: Maple Leafs och Canucks hämtade i full upplösning i stället för 150 respektive 350 pixlar breda

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@
  * har innehållshash i namnet och cachas därför permanent.
  */
 
-const CACHE = 'upptackaren-v47';
+const CACHE = 'upptackaren-v48';
 
 /**
  * Stadsfotona ligger med stabila namn under cities/ och hämtas in redan vid
